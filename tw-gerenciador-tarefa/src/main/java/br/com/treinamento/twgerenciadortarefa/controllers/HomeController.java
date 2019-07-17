532e87319f1904c5bfc381e4b81adabe84a0841d
Micaele Marilia <44231841+MicaeleMarilia@users.noreply.github.com>
2019-07-16 21:46:33 -0300

    Add files via upload

--- tw-gerenciador-tarefa/src/main/java/br/com/treinamento/twgerenciadortarefa/controllers/HomeController.java
+++ tw-gerenciador-tarefa/src/main/java/br/com/treinamento/twgerenciadortarefa/controllers/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("home/home");
-		mv.addObject("mensagem","Mensagem do Controller!");
+//		mv.addObject("mensagem","Mensagem do Controller!");
 		return mv;
 	}
 
